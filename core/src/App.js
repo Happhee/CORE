9c70fe7a2cf78d60a0f6a33ac4b6f944eca2fa90
seohee0112 <ghdtj7@gmail.com>
2021-10-22 22:54:21 +0900

    [ADD] 이벤트 리스너 추가

--- core/src/App.js
+++ core/src/App.js
@@ -1,13 +1,33 @@
-import logo from './logo.svg';
+/*eslint-disable */
 import './App.css';
+import React, { useState } from 'react';
+//내장 함수를 가져다 쓰겠습니다~
 
 function App() {
+  let [title, title_modify] = useState(['Student', 'WorkBook', 'FeedBack', 'My']); // [array , fun ]
+  //state데이터, state데이터 변경 함수 
+
   return (
     <div className="App">
       <div className="black-nav">
-        리액트 적응하기 1일차
+        CORE
       </div>
-      <h4> 블로그 글 </h4>
+      <div className="list" >
+        <h2 onClick={() => { console.log(1) }}> {title[0]}  </h2>
+        <hr />
+      </div>
+      <div className="list" >
+        <h2> {title[1]}</h2>
+        <hr />
+      </div>
+      <div className="list" >
+        <h2> {title[2]}</h2>
+        <hr />
+      </div>
+      <div className="list" >
+        <h2> {title[3]}</h2>
+        <hr />
+      </div>
     </div>
   );
 }
