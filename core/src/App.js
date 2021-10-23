90a10f38480bc46a5a3485c14b042afe77f851e4
seohee0112 <ghdtj7@gmail.com>
2021-10-23 10:15:35 +0900

    [ADD] 모달 추가

--- core/src/App.js
+++ core/src/App.js
@@ -7,29 +7,57 @@ function App() {
   let [title, title_modify] = useState(['Student', 'WorkBook', 'FeedBack', 'My']); // [array , fun ]
   //state데이터, state데이터 변경 함수 
 
+
+  function Title_modify() {
+    var newArray = [...title];
+    newArray[0] = 'Not - Student ';
+    title_modify(newArray);
+  }
+
   return (
     <div className="App">
       <div className="black-nav">
         CORE
       </div>
+
       <div className="list" >
-        <h2 onClick={() => { console.log(1) }}> {title[0]}  </h2>
+        <h2 onClick={Title_modify}> {title[0]}  </h2>
         <hr />
       </div>
+
       <div className="list" >
         <h2> {title[1]}</h2>
         <hr />
       </div>
+
       <div className="list" >
         <h2> {title[2]}</h2>
         <hr />
       </div>
+
       <div className="list" >
         <h2> {title[3]}</h2>
         <hr />
       </div>
+
+      <Modal />
+
     </div>
+
   );
+
+}
+
+function Modal() {
+  return (
+    <>
+      <div className="modal">
+        <h3>제목</h3>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+    </>
+  )
 }
 
 export default App;
