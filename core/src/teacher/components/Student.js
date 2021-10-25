840c96eeb644f30655b05c360e663d7562d959d6
seohee0112 <ghdtj7@gmail.com>
2021-10-25 18:32:04 +0900

    [ADD] router 추가

--- core/src/teacher/components/Student.js
+++ core/src/teacher/components/Student.js
@@ -1,2 +1,14 @@
-import '../../css/teacher/FeedBack.css'
+import '../css/Student.css'
+import React, { Component } from 'react';
 
+class Student extends Component {
+    render() {
+        return (
+            <div>
+                <p>여기인가?</p>
+            </div>
+        );
+    }
+}
+
+export default Student;
