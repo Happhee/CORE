88731524a84365e1cedf1245239ccddb6440105f
seohee0112 <ghdtj7@gmail.com>
2021-10-26 10:42:23 +0900

    [ADD] 로그인 페이지 로그인 성공 컴포넌트 분리

--- core/src/App.js
+++ core/src/App.js
@@ -1,10 +1,10 @@
 /*eslint-disable */
 import './App.css';
 import React, { Component } from 'react';
-// import {Route, Switch, useHistory} from 'react-router-dom';
 
 import Toolbar from './components/Toolbar';
-import Subject from './components/Subject'
+import Login_Success from './components/Login_Success';
+
 
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
@@ -21,16 +21,6 @@ class App extends Component {
       mode: 'teacher',
       teacher_top: { feed: 'WorkBank' },
       student_top: { feed: 'FeedBank' },
-
-      teacher_subject: [
-        { id: 1, title: 'Student' },
-        { id: 2, title: 'WorkBook' },
-        { id: 3, title: 'FeedBack' }
-      ],
-
-      student_subject: [
-        { id: 1, title: 'WorkBook' }
-      ],
     }
   }
   //state데이터, state데이터 변경 함수 
@@ -42,18 +32,17 @@ class App extends Component {
 
     if (this.state.mode == 'teacher') {
       _feed = this.state.teacher_top.feed;
-      _subject = this.state.teacher_subject;
     }
     else {
       _feed = this.state.student_top.feed;
-      _subject = this.state.student_subject;
     }
+
 
     return (
       <Router className="App">
         <div>
           <Toolbar data={this.state.init} />
-          <Subject mode={this.state.mode} data={_subject} />
+          {/* <Login_Success mode={this.state.mode} /> */}
 
         </div>
 
