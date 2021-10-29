03933ea1a510db60353fb5df8b9d8d931eeb9781
seohee0112 <ghdtj7@gmail.com>
2021-10-29 11:44:01 +0900

    [ADD] App.js 정리

--- core/src/App.js
+++ core/src/App.js
@@ -2,7 +2,6 @@
 import './App.css';
 import React, { useState } from 'react';
 
-import Toolbar from './components/Toolbar';
 import Login from './components/Login';
 import SignUp from './components/SignUp'
 import ClassRoom from './components/ClassRoom';
@@ -11,7 +10,7 @@ import My from './components/My';
 import WorkBank from './teacher/components/WorkBank';
 import FeedBank from './student/components/FeedBank';
 import Information from './components/Information';
-import Subject from './components/Subject';
+
 
 
 import { BrowserRouter as Router, Route, Switch, Link, Redirect } from 'react-router-dom';
@@ -21,26 +20,11 @@ import { BrowserRouter as Router, Route, Switch, Link, Redirect } from 'react-ro
 function App(props) {
   //최상단 툴바 및 모드 설정 
 
-  let toolbar = [
-    { id: 1, title: 'Login', link: '/' },
-    { id: 2, title: 'Sign Up', link: '/signup' }];
-  let [mode, setMode] = useState('teacher');
-
-  //state데이터, state데이터 변경 함수 
-
-  let [teacher_subject, setTeacher] = useState([
-    { id: 1, title: 'Student' },
-    { id: 2, title: 'WorkBook' },
-    { id: 3, title: 'FeedBack' }])
-
 
   return (
     <Router>
       {/* 서히 시작 */}
       <div className="App">
-
-        <Toolbar data={toolbar} />
-
         <Switch>
           <Route exact={true} path='/' render={(props) => <Login {...props} />} />
           <Route path='/signup' render={(props) => <SignUp {...props} />} />
