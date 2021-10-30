9c6fc21b2006dfc7245fffdd644c71914093806d
seohee0112 <ghdtj7@gmail.com>
2021-10-30 22:40:20 +0900

    [ADD] 리스트박스추가

--- core/src/components/CoreDialog.js
+++ core/src/components/CoreDialog.js
@@ -6,7 +6,10 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+
+
 import { withStyles } from '@material-ui/core/styles';
+import CoreListbox from './CoreListbox';
 
 const CoreDialog = props => {
 
@@ -16,21 +19,31 @@ const CoreDialog = props => {
         fileName: '',
         open: false
     })
+    //입력값 리스트
     let [text_datas, setText_datas] = useState(props.text_data);
 
+
+    //강의실 리스트
+    let select_listbox = null;
+    let [listbox_datas, setListbox_datas] = useState(props.listbox_datas)
+    if (listbox_datas != null) {
+        select_listbox = <CoreListbox listbox_datas={listbox_datas} />
+    }
     function handleClickOpen() {
         setDialog_data({ open: true });
     }
 
     function handleClose() {
         setDialog_data({ file: null, fileName: '', open: false });
-        const newText_data = [...text_datas];
+        if (text_datas != null) {
+            const newText_data = [...text_datas];
 
-        for (let index = 0; index < text_datas.length; index++) {
-            newText_data[index].value = '';
+            for (let index = 0; index < text_datas.length; index++) {
+                newText_data[index].value = '';
 
+            }
+            setText_datas(newText_data);
         }
-        setText_datas(newText_data);
     }
 
     const handleValueChange = (e) => {
@@ -44,22 +57,30 @@ const CoreDialog = props => {
         newText_data[index].value = value;
         setText_datas(newText_data);
     }
-    const textfield_list = text_datas.map((text_data, index) =>
-        <div key={text_data.id}>
-            <TextField key={index} label={text_data.label} type="text" name={text_data.name} value={text_data.value} onChange={handleValueChange} /><br />
-        </div>
+    let textfield_list = null;
+    if (text_datas != null) {
+        textfield_list = text_datas.map((text_data, index) =>
+            <div key={text_data.id}>
+                <TextField key={index} label={text_data.label} type="text" name={text_data.name} value={text_data.value} onChange={handleValueChange} /><br />
+            </div>
 
-    )
+        )
+    }
+
+
     return (
-        <div>
+        <div className={props.button_box_div}>
             <div className={props.button_box}>
                 <button className={props.button_type} onClick={handleClickOpen}>{props.button_value}</button>
             </div>
             <Dialog open={dialog_data.open} onClose={handleClose}>
 
-                <DialogTitle >{props.dialog_title}</DialogTitle>
+                <DialogTitle >
+                    {props.dialog_title}
+                </DialogTitle>
 
                 <DialogContent>
+                    {select_listbox}
                     {textfield_list}
                 </DialogContent>
 
--- core/src/components/Unit.js
+++ core/src/components/Unit.js
@@ -5,8 +5,22 @@ import CoreDialog from './CoreDialog';
 class Customer extends React.Component {
     render() {
         let edit = null;
+        let listbox_datas = [
+            { id: 1, value: "C프로그래밍 및 실습" },
+            { id: 2, value: "자료구조 및 실습" },
+            { id: 3, value: "컴퓨터구조" },
+            { id: 4, value: "운영체제" }
+        ]
         if (this.props.edit) {
             console.log(this.props.edit);
+            edit = [<TableCell>
+                <CoreDialog key="add" button_box_div="add_problem_class_box" button_box="add_problem_class_box" button_value="등록"
+                    dialog_title="문제 등록하기" listbox_datas={listbox_datas} />
+                <button >수정</button>
+                <button >삭제</button>
+            </TableCell>];
+
+
         }
 
         return (
