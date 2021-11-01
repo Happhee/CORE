b1737a5b7414156c5ae3233f43b696d8e55b7fc9
seohee0112 <ghdtj7@gmail.com>
2021-11-01 19:21:28 +0900

    [UPGRADE] 워크뱅크 오류 수정

--- core/src/components/Unit.js
+++ core/src/components/Unit.js
@@ -2,14 +2,6 @@ import React from 'react';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import CoreDialog from './CoreDialog';
-
-class Unit extends React.Component {
-    render() {
-        return (
-            <TableRow>
-                <TableCell>{this.props.id}</TableCell>
-                <TableCell><button>{this.props.name}</button></TableCell>
-                <TableCell>{this.props.count}</TableCell>
 class Customer extends React.Component {
     render() {
         let edit = null;
@@ -42,6 +34,6 @@ class Customer extends React.Component {
     }
 }
 
-export default Unit;
+export default Customer;
 
 
