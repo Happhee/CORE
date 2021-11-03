9dc76e7f1ba19a0412aca37e3db334b8362bc3ad
seohee0112 <ghdtj7@gmail.com>
2021-11-03 12:58:23 +0900

    [ADD] 박스 추가하기

--- core/src/components/ProblemGrid.js
+++ core/src/components/ProblemGrid.js
@@ -6,16 +6,15 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
-import FormControl from '@mui/material/FormControl';
-import { InputLabel } from '@mui/material';
+
 
 //그리드 박스 스타일 속성 적용
 const Item = styled(Paper)(({ theme }) => ({
     ...theme.typography.body2,
-    padding: theme.spacing(0.5),
+    padding: theme.spacing(0),
     textAlign: 'start',
     color: theme.palette.text.secondary,
-    margin: theme.spacing(2),
+    margin: theme.spacing(1),
     boxShadow: 'none'
 }));
 
@@ -34,7 +33,7 @@ const CssTextField = styled(TextField)({
             borderColor: '#9A30AE',
         },
         '&:hover fieldset': {
-            borderColor: 'yellow',
+            borderColor: 'pink',
         },
         '&.Mui-focused fieldset': {
             borderColor: 'green',
@@ -46,30 +45,59 @@ function ProblemGrid(props) {
     let [grid_data, setGrid_data] = useState(props.grid_data);
 
 
-
     return (
-
-
-
         <Box sx={{ flexGrow: 1 }}>
             <Grid container spacing={2}>
 
-
-                <Grid item xs={8}>
-                    <Item>xs=8</Item>
-                </Grid>
-                <Grid item xs={4}>
-                    <Item>xs=4</Item>
-                </Grid>
-                <Grid item xs={4}>
-
+                {/* 상단 문제 챕터 및 단원 */}
+                <Grid item xs={2}>
                     <Item>
-                        <p>문제</p>
-                        <CssTextField label="Custom CSS" variant="outlined" id="custom-css-outlined-input" />
+                        <p className="grid_data_title">{grid_data[0].title}</p>
+                        <CssTextField label={grid_data[0].input} variant="outlined" id="custom-css-outlined-input" />
                     </Item>
                 </Grid>
-                <Grid item xs={8}>
-                    <Item>xs=8</Item>
+                <Grid item xs={2}>
+                    <Item>
+                        <p className="grid_data_title">{grid_data[1].title}</p>
+                        <CssTextField label={grid_data[1].input} variant="outlined" id="custom-css-outlined-input" />
+                    </Item>
+                </Grid>
+            </Grid>
+
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <Item>
+                        <p className="grid_data_title">{grid_data[2].title}</p>
+                        <CssTextField label={grid_data[2].input} variant="outlined" id="custom-css-outlined-input" />
+                    </Item>
+                </Grid>
+                <Grid item xs={12}>
+                    <Item>
+                        <p className="grid_data_title">{grid_data[3].title}</p>
+                        <CssTextField label={grid_data[3].input} variant="outlined" id="custom-css-outlined-input" />
+                    </Item>
+
+                </Grid>
+                <Grid item xs={12}>
+
+                    <Item>
+                        <p className="grid_data_title">{grid_data[4].title}</p>
+                        <CssTextField label={grid_data[4].input} variant="outlined" id="custom-css-outlined-input" />
+                    </Item>
+                </Grid>
+                <Grid item xs={6}>
+
+                    <Item>
+                        <p className="grid_data_title">{grid_data[5].title}</p>
+                        <CssTextField label={grid_data[5].input} variant="outlined" id="custom-css-outlined-input" />
+                    </Item>
+                </Grid>
+                <Grid item xs={6}>
+
+                    <Item>
+                        <p className="grid_data_title">{grid_data[6].title}</p>
+                        <CssTextField label={grid_data[6].input} variant="outlined" id="custom-css-outlined-input" />
+                    </Item>
                 </Grid>
             </Grid>
         </Box>
--- core/src/teacher/components/ProblemAdd.js
+++ core/src/teacher/components/ProblemAdd.js
@@ -4,14 +4,14 @@ import ProblemGrid from '../../components/ProblemGrid';
 
 function ProblemAdd() {
 
-    let [grid_data, setGrid_data] = useState(
+    let [grid_data, setGrid_data] = useState([
         { id: 1, title: "Problem - Chapter", input: "1단원" },
         { id: 2, title: "Problem - Number", input: "2번" },
         { id: 3, title: "Problem Title", input: "문제명" },
         { id: 4, title: "Problem Description", input: "문제 상세 설명" },
         { id: 5, title: "Answer Code", input: "정답 코드" },
         { id: 6, title: "Input", input: "입력값" },
-        { id: 7, title: "Output", input: "결과값" },
+        { id: 7, title: "Output", input: "결과값" }]
     )
     return (
         <div className="main_div">
