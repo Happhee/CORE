da158c70bc4a71e472d90db46cf5bf80ff64d382
hyunjin <jhj981102@gmail.com>
2021-11-08 19:35:39 +0900

    [add]툴바에 아이콘 추가~

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import '../css/Toolbar.css'
 import { Link } from 'react-router-dom';
+import { margin, width } from '@mui/system';
 
 
 //상단바 구성하고, 맨처음은 로그인 띄워주기
@@ -12,7 +13,10 @@ const Toolbar = props => {
         <div>
             <div className="tool_bar">
                 <Link to={data[1].link} className="text_style">{data[1].title}</Link>
+                <img style={{ width: '30px',height:'28px',marginRight:'10px'}} src={require('../image/iconWorkbank.png').default} />
                 <Link to={data[0].link} className="text_style"> {data[0].title}</Link>
+                <img style={{ width: '30px',height:'28px',marginRight:'10px'}} src={require('../image/iconMy.png').default} />
+
             </div>
         </div>
     );
