1ec63194c212e9cfbbfa294db759ebadcb0f2aba
seohee0112 <ghdtj7@gmail.com>
2021-11-12 13:06:03 +0900

    [ADD] 쿠키저장

--- core/server/models/User-model.js
+++ core/server/models/User-model.js
@@ -1,14 +1,19 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
     id: {
         type: String,
-        required: true
+        required: true,
+        maxlength: 100
     },
     pw: {
         type: String,
+        minlength: 10
     },
     name: {
         type: String,
@@ -21,6 +26,34 @@ const userSchema = new Schema({
     },
     classs: {
         type: Array,
+    },
+    token: {
+        type: String
+    },
+    tokenExp: {
+        type: Number
+    }
+})
+
+//회원가입 시 저장전에 디비에 저장될 비밀번호 암호화
+userSchema.pre('save', function (next) {
+    let user = this;
+
+    if (user.isModified('pw')) {
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if (err) {
+                return next(err)
+            }
+            bcrypt.hash(user.pw, salt, function (err, hash) {
+                if (err) {
+                    return next(err)
+                }
+                user.pw = hash
+                next();
+            })
+        })
+    } else {
+        next()
     }
 })
 
