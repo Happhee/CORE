c6f166b4d8fee5872b0cb749f490b1dce7e0aa4b
seohee0112 <ghdtj7@gmail.com>
2021-11-19 23:30:36 +0900

    cxc

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express=require('express');
-const app=express();
-const path =require('path');
+const express = require('express');
+const app = express();
+const path = require('path');
 
-const port=5000;
+const port = "5000";
 app.listen(port);
 
 //리액트 정적 파일 제공
-app.use(express.static(path.join(__dirname,'core/build')));
+app.use(express.static(path.join(__dirname, 'core/build')));
 
 //라우트 설정
-app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname+'/core/build/index.html'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/core/build/index.html'));
 
 });
 
